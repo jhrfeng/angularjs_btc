6888e3766285964a83376846d880f2adf14a9d69
chungong <chun.gong@pactera.com>
2017-03-29 23:13:13 +0800

    我的信息页面添加

--- js/controllers/MeController.js
+++ js/controllers/MeController.js
@@ -3,4 +3,13 @@ app.controller('MeController',
 function($rootScope, $scope) {
 	
   $rootScope.header = true;
+  $scope.userName = "John";
+  $scope.passWord = "123456";
+  $scope.payPassWord = "123456";
+  
+  $scope.tabshow = 1;
+  
+  $scope.toChangeView = function(type) {
+	$scope.tabshow = type;
+	}
 }]);
